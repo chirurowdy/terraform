d706b358abcababaedb94a83cc4b2554026bfb4a
chirurowdy <chiranjeevikeerthi77777@gmail.com>
2025-03-26 15:15:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "one" {
-  count = 2
+  count = 3
   ami           = "ami-08b5b3a93ed654d19"
   instance_type = "t2.micro"
   tags = {
